86ec0869fbffe188b461237fb3266aaa66eac265
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-04-11 21:12:10 -0500

    Adding React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-const App = () => {
-  const text = 'Welcome to my React Template'
-  return <h1>{text}</h1>
-}
+import PageNotFound from 'components/pages/PageNotFound'
+import About from 'components/pages/About'
+import Home from 'components/pages/Home'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="*">
+        <PageNotFound />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
---
+++ src/components/pages/About.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Go to Home</Link>
+  </div>
+)
+
+export default About
